e4ac341dd59c88128023dd7906c3495ee606ac27
Anton Fishchev <antonfishevv@gmail.com>
2024-07-03 12:51:07 +0300

    Добавил валидацию и изменил расчет площади круглой столешницы

--- MaterialСalculator/Entities/CircularForm.cs
+++ MaterialСalculator/Entities/CircularForm.cs
@@ -4,11 +4,21 @@ namespace MaterialСalculator.Entities
 {
 	public class CircularForm : IArea
 	{
-		public double Diameter { get; set; }
+		public double Diameter { get; private set; }
+
+		public CircularForm(double diameter)
+		{
+			if (diameter <= 0)
+			{
+				throw new ArgumentException(nameof(diameter));
+			}
+
+			Diameter = diameter;
+		}
 
 		public double CalculateArea()
 		{
-			return Math.PI * Math.Pow(Diameter / 2, 2);
+			return (Math.PI * Math.Pow(Diameter, 2)) / 4;
 		}
 	}
 }
